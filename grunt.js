4660bf8f23ba7d0fc0323c689440f956730ce049
tomas2387 <tomas2387@gmail.com>
2013-07-18 00:24:01 +0200

    Reset Gruntfile back to normal

--- grunt.js
+++ grunt.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         shell: {
             'mocha-phantomjs': {
-                command: 'mocha-phantomjs http://localhost/webrtcApp/test/testRunner.html',
+                command: 'mocha-phantomjs http://localhost:8000/webrtcApp/scripts/test/testRunner.html',
                 options: {
                     stdout: true,
                     stderr: true
